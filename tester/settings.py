4df5af39e732be2aa43171aac1e0b3d3b27d8f69
Tabong-Ang <tabongphilip@gmail.com>
2025-09-21 15:42:08 +0100

    added ssl_require to db

--- tester/settings.py
+++ tester/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'tester.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
